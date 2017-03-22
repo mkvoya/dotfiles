ad5ab14815a12bfa5c9f1772ccd113ee7a8c51d1
Mingkai Dong <mingkaidong@gmail.com>
2017-03-22 14:44:15 +0800

    feat(hammerspoon): Add MacOS hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,109 @@
+
+
+masterkey = {"alt"}
+slavekey = {"alt", "shift"}
+hs.hotkey.bind(masterkey, "R", function()
+    hs.reload()
+end)
+hs.alert.show("Config loaded")
+
+-- hs.notify.new({title="Hammerspoon", informativeText="Config Reloaded"}):send()
+--
+-- some nethack moves
+hs.hotkey.bind(masterkey, "K", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    f.y = f.y - 10
+    win:setFrame(f)
+end)
+hs.hotkey.bind(masterkey, "H", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    f.x = f.x - 10
+    win:setFrame(f)
+end)
+hs.hotkey.bind(masterkey, "L", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    f.x = f.x + 10
+    win:setFrame(f)
+end)
+hs.hotkey.bind(masterkey, "J", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    f.y = f.y + 10
+    win:setFrame(f)
+end)
+-- window resizing
+hs.hotkey.bind(slavekey, "H", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+hs.hotkey.bind(slavekey, "J", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y + max.h / 2
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+hs.hotkey.bind(slavekey, "K", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+hs.hotkey.bind(slavekey, "L", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + max.w / 2
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+hs.hotkey.bind(slavekey, "F", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+end)
+hs.hotkey.bind(slavekey, "R", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + max.w / 6
+    f.y = max.y + max.h / 6
+    f.w = max.w * 2 / 3
+    f.h = max.h * 2 / 3
+    win:setFrame(f)
+end)
