2a9c1cdbd7f1646ee21fc6837cfeceeefa9b19c3
Mingkai Dong <mingkaidong@gmail.com>
2017-03-25 23:02:46 +0800

    fix(emacs.d): Add fonts and powerline to emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -118,6 +118,8 @@
      (insert-char (cdr (assoc-string, unicode-name (ucs-names))))))
 (bind-key "C-x 8 s" (my/insert-unicode "ZERO WIDTH SPACE"))
 (bind-key "C-x 8 S" (my/insert-unicode "SNOWMAN"))
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
 
 ; Clean up spaces
 (bind-key "M-SPC" 'cycle-spacing)
@@ -198,17 +200,98 @@
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
+
+(use-package monokai-theme
+  :ensure t)
+(load-theme 'monokai t)
+
+;(use-package spaceline-config
+;  :ensure t
+;  :config
+;  (spaceline-spacemacs-theme))
+
+(use-package airline-themes
+  :ensure t)
+(load-theme 'airline-light t)
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(custom-safe-themes
+   (quote
+    ("3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "962dacd99e5a99801ca7257f25be7be0cebc333ad07be97efd6ff59755e6148f" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" default)))
+ '(fci-rule-color "#3C3D37")
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100))))
+ '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (powerline use-package miniedit guide-key evil company color-theme-solarized))))
+    (powerline use-package miniedit guide-key evil company color-theme-solarized)))
+ '(pos-tip-background-color "#A6E22E")
+ '(pos-tip-foreground-color "#272822")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;;;; INSTALL FILES
+;(call-process "git" nil t nil "clone" "https://github.com/powerline/fonts.git" "/tmp/fonts")
+;(call-process "ls" nil t nil "/tmp/fonts")
+;(call-process "/tmp/fonts/install.sh" nil t)
+
+;;; FONT
+(add-to-list 'default-frame-alist '(font . "3270"))
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono for Powerline"))
+
+(set-face-attribute 'default nil :family "DejaVu Sans Mono for Powerline")
+;(set-face-attribute 'default nil :height 94)
+(set-fontset-font "fontset-default" 'unicode "DejaVu Sans Mono for Powerline")
+
+;;
+
+(set-face-attribute 'mode-line nil :family "DejaVu Sans Mono for Powerline")
+(set-face-attribute 'mode-line-inactive nil :family "DejaVu Sans Mono for Powerline")
+(set-face-attribute 'powerline-inactive1 nil :family "DejaVu Sans Mono for Powerline")
+(set-face-attribute 'powerline-active1 nil :family "DejaVu Sans Mono for Powerline")
