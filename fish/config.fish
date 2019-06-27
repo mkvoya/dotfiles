2ff5d6f4488a7b14ed87fb5c787175b9ea6b0c01
Mingkai Dong <mingkaidong@gmail.com>
2019-06-27 09:17:53 +0800

    reenable fisher and add fzf

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,27 @@
 
-## fisher package manager
-#if not functions -q fisher
-#    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-#    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-#    fish -c fisher
-#end
+# fisher package manager
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# fzf
+function fzf-cdhist-widget -d 'cd to one of the previously visited locations'
+	# Clear non-existent folders from cdhist.
+	set -l buf
+	for i in (seq 1 (count $dirprev))
+		set -l dir $dirprev[$i]
+		if test -d $dir
+			set buf $buf $dir
+		end
+	end
+	set dirprev $buf
+	string join \n $dirprev | tac | sed 1d | eval (__fzfcmd) +m --tiebreak=index --toggle-sort=ctrl-r $FZF_CDHIST_OPTS | read -l result
+	[ "$result" ]; and cd $result
+	commandline -f repaint
+end
+
 
 # vi key binding
 function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
